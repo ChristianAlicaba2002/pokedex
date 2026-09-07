1e13429121be6684c185c588721bfae720465f6f
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-09-07 21:10:22 +0800

    feat: Integrate React Query for data fetching by wrapping TabLayout in QueryClientProvider to enhance state management

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,15 +4,20 @@ import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
+    <QueryClientProvider client={queryClient}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
       <AppTabs />
     </ThemeProvider>
+    </QueryClientProvider>
   );
 }

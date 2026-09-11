7598755d0abeb172a0fe76bf33e1c1ebc3aee5cd
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-09-11 10:18:44 +0800

    refactor: Update AppTabs component to rename tabs for improved clarity and navigation consistency

--- src/components/app-tabs.web.tsx
+++ src/components/app-tabs.web.tsx
@@ -1,13 +1,13 @@
 import {
-  Tabs,
   TabList,
-  TabTrigger,
-  TabSlot,
-  TabTriggerSlotProps,
   TabListProps,
+  Tabs,
+  TabSlot,
+  TabTrigger,
+  TabTriggerSlotProps,
 } from 'expo-router/ui';
 import { SymbolView } from 'expo-symbols';
-import { Pressable, useColorScheme, View, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, useColorScheme, View } from 'react-native';
 
 import { ExternalLink } from './external-link';
 import { ThemedText } from './themed-text';
@@ -21,10 +21,10 @@ export default function AppTabs() {
       <TabSlot style={{ height: '100%' }} />
       <TabList asChild>
         <CustomTabList>
-          <TabTrigger name="home" href="/" asChild>
-            <TabButton>Home</TabButton>
+          <TabTrigger name="index" href="/" asChild>
+            <TabButton>Pokedex</TabButton>
           </TabTrigger>
-          <TabTrigger name="explore" href="/explore" asChild>
+          <TabTrigger name="pokemon" href="/pokemon" asChild>
             <TabButton>Explore</TabButton>
           </TabTrigger>
         </CustomTabList>

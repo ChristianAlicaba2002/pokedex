615337f00c4d9ddb7df214cc93ccd3b8ab8afbbd
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-09-11 10:18:37 +0800

    feat: Update AppTabs component to include Pokedex, Favorites, and More tabs with MaterialCommunityIcons for enhanced navigation

--- src/components/app-tabs.tsx
+++ src/components/app-tabs.tsx
@@ -1,3 +1,4 @@
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { NativeTabs } from 'expo-router/unstable-native-tabs';
 import { useColorScheme } from 'react-native';
 
@@ -8,25 +9,47 @@ export default function AppTabs() {
   const colors = Colors[scheme === 'unspecified' ? 'light' : scheme];
 
   return (
-    <NativeTabs
-      backgroundColor={colors.background}
-      indicatorColor={colors.backgroundElement}
-      labelStyle={{ selected: { color: colors.text } }}>
-      <NativeTabs.Trigger name="index">
-        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/home.png')}
-          renderingMode="template"
-        />
-      </NativeTabs.Trigger>
+    <>
+      {/* PokedexScreen */}
+      <NativeTabs
+        backgroundColor={colors.background}
+        indicatorColor={colors.backgroundElement}
+        labelStyle={{ selected: { color: colors.text } }}>
+        <NativeTabs.Trigger name="index">
+          <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name='home' />}
+            renderingMode="template"
+          />
+        </NativeTabs.Trigger>
 
-      <NativeTabs.Trigger name="explore">
-        <NativeTabs.Trigger.Label>Explore</NativeTabs.Trigger.Label>
-        <NativeTabs.Trigger.Icon
-          src={require('@/assets/images/tabIcons/explore.png')}
-          renderingMode="template"
-        />
-      </NativeTabs.Trigger>
-    </NativeTabs>
+        {/* PokedexScreen */}
+        <NativeTabs.Trigger name="pokemon">
+          <NativeTabs.Trigger.Label>Pokedex</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="pokeball" />}
+            renderingMode="template"
+          />
+        </NativeTabs.Trigger>
+
+        {/* FavoritesScreen */}
+        <NativeTabs.Trigger name="favorites">
+          <NativeTabs.Trigger.Label>Favorites</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="heart" />}
+            renderingMode="template"
+          />
+        </NativeTabs.Trigger>
+
+        {/* MoreScreen */}
+        <NativeTabs.Trigger name="settings">
+          <NativeTabs.Trigger.Label>More</NativeTabs.Trigger.Label>
+          <NativeTabs.Trigger.Icon
+            src={<NativeTabs.Trigger.VectorIcon family={MaterialCommunityIcons} name="dots-vertical" />}
+            renderingMode="template"
+          />
+        </NativeTabs.Trigger>
+      </NativeTabs>
+    </>
   );
 }

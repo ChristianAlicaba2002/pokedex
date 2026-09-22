3034702587934fef96ab6a8e2f03e0cf568c0d6f
ChristianAlicaba2002 <christiandave120702@gmail.com>
2026-09-22 20:39:30 +0800

    feat: Replace AnimatedSplashOverlay with PokemonSplashOverlay and adjust splash screen options for improved user experience

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,20 +2,21 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import { PokemonSplashOverlay } from '@/components/pokemon-splash';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 SplashScreen.preventAutoHideAsync();
+SplashScreen.setOptions({ duration: 400, fade: true });
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
     <QueryClientProvider client={queryClient}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
+      <PokemonSplashOverlay />
       <AppTabs />
     </ThemeProvider>
     </QueryClientProvider>
